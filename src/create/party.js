c5927d6a8fb8c017d284700152befdcded345573
vatsalkgor <vatsalkgor@gmail.com>
2020-05-24 11:43:10 +1200

    party api created

--- src/create/party.js
+++ src/create/party.js
@@ -2,8 +2,52 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
-router.post("/create/party/add", (req, res) => {
-    res.json({ msg: "hi" });
+const insertParty =
+    "INSERT INTO party(name,type,contact,pending_amount) VALUES (?,?,?,?)";
+const updateParty =
+    "UPDATE party SET name=?, type=?, contact=?,pending_amount=? WHERE id=?";
+const deleteParty = "DELETE FROM party where id=?";
+const getParty = "SELECT * FROM party";
+const getPartyWithId = "SELECT * from party where id=?";
+
+router.post("/create/party", (req, res) => {
+    const info = db
+        .prepare(insertParty)
+        .run(
+            req.body.name,
+            req.body.type,
+            req.body.contact,
+            req.body.pending_amount
+        );
+    res.json(info);
+});
+
+router.get("/create/party", (req, res) => {
+    const info = db.prepare(getParty).all();
+    res.json(info);
+});
+
+router.get("/create/party/:id", (req, res) => {
+    const info = db.prepare(getPartyWithId).get(req.params.id);
+    res.json(info);
+});
+
+router.put("/create/party/:id", (req, res) => {
+    const info = db
+        .prepare(updateParty)
+        .run(
+            req.body.name,
+            req.body.type,
+            req.body.contact,
+            req.body.pending_amount,
+            req.params.id
+        );
+    res.json(info);
+});
+
+router.delete("/create/party/:id", (req, res) => {
+    const info = db.prepare(deleteParty).run(req.params.id);
+    res.json(info);
 });
 
 module.exports = router;
